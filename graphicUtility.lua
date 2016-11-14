532ad504dea035d53fd16a4a80d5c1cf62b11bd7
samson chen <samsonchen1989@gmail.com>
2016-11-14 15:17:56 +0800

    Add polygon joint check function.

--- graphicUtility.lua
+++ graphicUtility.lua
@@ -1,44 +1,118 @@
+-- Ref: http://www.phailed.me/2011/02/polygonal-collision-detection/
+-- vectors are just lists
+-- segments are tables: {start = vect, end = vect, dir = vect}
+-- polygons includes {vertices = {vectors}, edge = {vectors}}
+
+function vec(x, y)
+	return {x, y}
+end
+
+v = vec -- shortcut
+
+function dot(v1, v2)
+	return v1[1]*v2[1] + v1[2]*v2[2]
+end
+
+function normalize(v)
+	local mag = math.sqrt(v[1]^2 + v[2]^2)
+	return vec(v[1]/mag, v[2]/mag)
+end
+
+function perp(v)
+	return {v[2],-v[1]}
+end
+
+function segment(a, b)
+	local obj = {a=a, b=b, dir={b[1] - a[1], b[2] - a[2]}}
+	obj[1] = obj.dir[1]; obj[2] = obj.dir[2]
+	return obj
+end
+
+function polygon(vertices)
+	local obj = {}
+	obj.vertices = vertices
+	obj.edges = {}
+	for i=1,#vertices do
+		table.insert(obj.edges, segment(vertices[i], vertices[1+i%(#vertices)]))
+	end
+	return obj
+end
+
+function project(a, axis)
+	axis = normalize(axis)
+	local min = dot(a.vertices[1],axis)
+	local max = min
+	for i,v in ipairs(a.vertices) do
+		local proj =  dot(v, axis) -- projection
+		if proj < min then min = proj end
+		if proj > max then max = proj end
+	end
+
+	return {min, max}
+end
+
+function contains(n, range)
+	local a, b = range[1], range[2]
+	if b < a then a = b; b = range[1] end
+	return n >= a and n <= b
+end
+
+function overlap(a_, b_)
+	if contains(a_[1], b_) then return true
+	elseif contains(a_[2], b_) then return true
+	elseif contains(b_[1], a_) then return true
+	elseif contains(b_[2], a_) then return true
+	end
+	return false
+end
+
+
+function sat(a, b)
+	for i,v in ipairs(a.edges) do
+		local axis = perp(v)
+		local a_, b_ = project(a, axis), project(b, axis)
+		if not overlap(a_, b_) then return false end
+	end
+	for i,v in ipairs(b.edges) do
+		local axis = perp(v)
+		local a_, b_ = project(a, axis), project(b, axis)
+		if not overlap(a_, b_) then return false end
+	end
+
+	return true
+end
+
 -- Check whether a point inside a polygon, lua version
 -- Ref: https://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
-local IsPointInPolygon = function(p, polygon)
-    local inside = false
+local isPointInPolygon = function(p, polygon)
+	local inside = false
 
-    local i = 1
-    local j = #polygon
+	local i = 1
+	local j = #(polygon.vertices)
 
-    while (i <= #polygon) do
-        if ( ((polygon[i].y > p.y) ~= (polygon[j].y > p.y)) and
-             (p.x < (polygon[j].x - polygon[i].x) * (p.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x)) then
-            inside = not inside
-        end
+	while (i <= #(polygon.vertices)) do
+		if ( ((polygon.vertices[i][2] > p[2]) ~= (polygon.vertices[j][2] > p[2])) and
+		     (p[1] < (polygon.vertices[j][1] - polygon.vertices[i][1]) * (p[2] - polygon.vertices[i][2]) / (polygon.vertices[j][2] - polygon.vertices[i][2]) + polygon.vertices[i][1])) then
+			inside = not inside
+		end
 
-        j = i
-        i = i + 1
-    end
+		j = i
+		i = i + 1
+	end
 
-    return inside
+	return inside
 end
 
-local test = {
-    {x = 3, y = 5},
-    {x = 3, y = 6},
-    {x = 2, y = 2},
-    {x = 4, y = 3},
-    {x = 6, y = 3},
-    {x = 5, y = 3},
-}
+local a = polygon{v(5, 3), v(-3, 6), v(-1, 3), v(-3, 1)}
+local b = polygon{v(3, 1), v(1, 5), v(7,5), v(5, 2)}
+local p = v(6, 4)
 
--- Clock-wise point list, otherwise may get opposite result
-local polygon = {
-    {x = 2, y = 2},
-    {x = 2, y = 5},
-    {x = 4, y = 6},
-    {x = 6, y = 2},
-}
+local main = function()
+	local time = os.clock()
 
-for k,v in pairs(test) do
-    print("x:"..v.x..", y:"..v.y)
-    print("Is in polygon:", IsPointInPolygon(v, polygon))
+	print("check poly joint result:", sat(a, b))
+	print("check point result:", isPointInPolygon(p, b))
 end
 
--- Ref: http://stackoverflow.com/questions/217578/how-can-i-determine-whether-a-2d-point-is-within-a-polygon
+
+main()
